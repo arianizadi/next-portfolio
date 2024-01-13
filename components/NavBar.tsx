0f4a4213ebc83e94bae6444fd9274d9987577703
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-13 15:30:39 -0800

    NavBar

---
+++ components/NavBar.tsx
@@ -0,0 +1,19 @@
+import { Geologica } from 'next/font/google'
+
+const font = Geologica({ subsets: ['latin'] })
+
+export function NavBar() {
+  return (
+    <main className={`flex justify-between text-center w-screen fixed items-center px-10 py-5 text-lg border-b ${font.className}`}>
+      <div className="flex">
+        <p>A</p>
+        <p>I</p>
+      </div>
+      <div className="flex space-x-10">
+        <a href="#home">Home</a>
+        <a href='#projects'>Projects</a>
+        <a href='#about'>About</a>
+      </div>
+    </main>
+  )
+}
