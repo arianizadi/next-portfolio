6335f45179ffe7e862901eea44c5debe428e41f3
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-13 19:17:43 -0800

    Clickable Logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,10 +5,10 @@ const font = Geologica({ subsets: ['latin'] })
 export function NavBar() {
   return (
     <main className={`flex justify-between text-center w-screen fixed items-center px-10 py-5 text-lg border-b ${font.className}`}>
-      <div className="flex">
+      <a href='/' className="flex">
         <p>A</p>
         <p>I</p>
-      </div>
+      </a>
       <div className="flex space-x-10">
         <a href="#home">Home</a>
         <a href='#projects'>Projects</a>
