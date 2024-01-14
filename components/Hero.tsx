e18d702b9e038575da5fceae626fcb97e15c00a4
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-13 19:04:32 -0800

    Mobile Friendly

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,9 +5,9 @@ export function Hero() {
   return (
     <main id='home'>
       <Image className='-z-10 brightness-75' src={earth} fill objectFit='cover' quality={100} alt='background image of ocean'></Image>
-      <div className="flex flex-col items-center justify-center w-full h-screen">
-        <Image className='absolute -z-10 top-[60%] md:top-[50%] md:left-[10%] brightness-75' id='astro' src="/astro.png" width={200} height={200} alt='astronaut model'></Image>
-        <Image className='absolute -z-10 bottom-[50%] md:bottom-[30%] md:right-[5%] brightness-75' id='sat' src="/sat.png" width={500} height={200} alt='astronaut model'></Image>
+      <div className="flex flex-col items-center text-center justify-center w-full h-screen">
+        <img className='absolute blur-[0px] h-[200px] -z-10 top-[60%] md:top-[50%] md:left-[10%] brightness-75' id='astro' src="/astro.png" alt='astronaut model'></img>
+        <img className='absolute blur-[0px] h-[250px] md:h-[600px] -z-10 bottom-[50%] md:bottom-[30%] md:right-[5%] brightness-75' id='sat' src="/sat.png" alt='astronaut model'></img>
         <h1 className="text-6xl font-bold">Arian Izadi</h1>
         <h2 className="text-2xl font-bold">Software Engineer & Security Researcher</h2>
       </div>
