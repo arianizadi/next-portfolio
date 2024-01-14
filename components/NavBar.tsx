627b11198cecf011451c9a5b13847edc37dabafe
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-13 19:22:40 -0800

    Updated Link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,14 +1,15 @@
 import { Geologica } from 'next/font/google'
+import Link from 'next/link'
 
 const font = Geologica({ subsets: ['latin'] })
 
 export function NavBar() {
   return (
     <main className={`flex justify-between text-center w-screen fixed items-center px-10 py-5 text-lg border-b ${font.className}`}>
-      <a href='/' className="flex">
+      <Link href='/' className="flex">
         <p>A</p>
         <p>I</p>
-      </a>
+      </Link>
       <div className="flex space-x-10">
         <a href="#home">Home</a>
         <a href='#projects'>Projects</a>
