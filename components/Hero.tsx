7c1fe42a1dbb53eb544cc18a1be34a4985b81160
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-13 17:05:58 -0800

    Mobile Hero Friendly

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image'
 export function Hero() {
   const earth = "https://images.unsplash.com/photo-1700173318258-3c0134576743"
   return (
-    <main>
+    <main id='home'>
       <Image className='-z-10 brightness-75' src={earth} fill objectFit='cover' quality={100} alt='background image of ocean'></Image>
       <div className="flex flex-col items-center justify-center w-full h-screen">
-        <Image className='absolute -z-10 top-[50%] left-[10%] brightness-75' id='astro' src="/astro.png" width={200} height={200} alt='astronaut model'></Image>
-        <Image className='absolute -z-10 bottom-[30%] right-[5%] brightness-75' id='sat' src="/sat.png" width={500} height={200} alt='astronaut model'></Image>
+        <Image className='absolute -z-10 top-[60%] md:top-[50%] md:left-[10%] brightness-75' id='astro' src="/astro.png" width={200} height={200} alt='astronaut model'></Image>
+        <Image className='absolute -z-10 bottom-[50%] md:bottom-[30%] md:right-[5%] brightness-75' id='sat' src="/sat.png" width={500} height={200} alt='astronaut model'></Image>
         <h1 className="text-6xl font-bold">Arian Izadi</h1>
         <h2 className="text-2xl font-bold">Software Engineer & Security Researcher</h2>
       </div>
