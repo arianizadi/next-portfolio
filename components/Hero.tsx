fd37f38e2faf68bb846cd5fee00d4d2169a60950
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-13 19:16:06 -0800

    Image Optimize

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 
 export function Hero() {
-  const earth = "https://images.unsplash.com/photo-1700173318258-3c0134576743"
   return (
     <main id='home'>
-      <Image className='-z-10 brightness-75' src={earth} fill objectFit='cover' quality={100} alt='background image of ocean'></Image>
+      <Image className='-z-10 brightness-75' src="/earth.jpg" fill objectFit='cover' quality={100} alt='background image of ocean'></Image>
       <div className="flex flex-col items-center text-center justify-center w-full h-screen">
-        <img className='absolute blur-[0px] h-[200px] -z-10 top-[60%] md:top-[50%] md:left-[10%] brightness-75' id='astro' src="/astro.png" alt='astronaut model'></img>
-        <img className='absolute blur-[0px] h-[250px] md:h-[600px] -z-10 bottom-[50%] md:bottom-[30%] md:right-[5%] brightness-75' id='sat' src="/sat.png" alt='astronaut model'></img>
+        <img className='absolute blur-[0px] h-[200px] -z-10 top-[60%] md:top-[50%] md:left-[10%] brightness-75' id='astro' src="/astro.webp" alt='astronaut model'></img>
+        <img className='absolute blur-[0px] h-[250px] md:h-[600px] -z-10 bottom-[50%] md:bottom-[30%] md:right-[5%] brightness-75' id='sat' src="/sat.webp" alt='astronaut model'></img>
         <h1 className="text-6xl font-bold">Arian Izadi</h1>
         <h2 className="text-2xl font-bold">Software Engineer & Security Researcher</h2>
       </div>
