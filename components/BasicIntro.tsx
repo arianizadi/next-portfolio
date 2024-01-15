c2fb6d068e57f69601810d6a9e83c580437ad2f6
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-14 19:43:24 -0800

    Added Links

--- components/BasicIntro.tsx
+++ components/BasicIntro.tsx
@@ -2,6 +2,11 @@ import Image from "next/image"
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect } from "react";
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -29,8 +34,13 @@ export function BasicIntro() {
         <p className="pt-2 max-w-xl">I am a senior studying Computer Science at the University of Nevada, Las Vegas. In addition to my academic pursuits, I currently serve as the President of Layer Zero, the cybersecurity club on campus. I am always eager to explore and create innovative solutions to problems, harnessing the power of ever-changing technology to make a difference.</p>
       </div>
       <img className="absolute h-[200px]" id="rocket" src="/spaceship.webp" alt="spaceship" />
-      <div>
-
+      <div className="pt-10">
+        <a href="https://github.com/arianizadi" rel="noreferrer" target={'_blank'}>
+          <FontAwesomeIcon icon={faGithub} className="text-white text-4xl mx-2" />
+        </a>
+        <a href="https://www.linkedin.com/in/arianizadi/" rel="noreferrer" target={'_blank'}>
+          <FontAwesomeIcon icon={faLinkedin} className="text-white text-4xl mx-2" />
+        </a>
       </div>
     </div>
   )
