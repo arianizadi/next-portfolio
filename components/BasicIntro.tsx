b9b605ab812abc5c6bb55b769e10b395668b0e7b
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-14 19:15:41 -0800

    Updated Text

--- components/BasicIntro.tsx
+++ components/BasicIntro.tsx
@@ -27,9 +27,7 @@ export function BasicIntro() {
         <Image src="/arian_fa22.jpg" alt="Arian Izadi Portrait" width={200} height={200} className="rounded-full border-2 z-10 border-white" />
         <div className="text-white text-center pt-10 px-2">
           <h1 className="text-2xl font-bold">My name is Arian!</h1>
-          <p className="pt-2 max-w-xl">I am a senior studying Computer Science at the University of Nevada, Las Vegas.
-            In addition to my academic pursuits, I serve as the President of Layer Zero, the cybersecurity club at UNLV.
-            I am always eager to explore and create new solutions, harnessing the power of technology to innovate and make a difference.</p>
+          <p className="pt-2 max-w-xl">I am a senior studying Computer Science at the University of Nevada, Las Vegas. In addition to my academic pursuits, I currently serve as the President of Layer Zero, the cybersecurity club on campus. I am always eager to explore and create innovative solutions to problems, harnessing the power of ever-changing technology to make a difference.</p>
         </div>
         <img className="absolute h-[200px]" id="rocket" src="/spaceship.webp" alt="spaceship" />
       </div>
