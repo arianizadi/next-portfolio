9291fd42b0e7e9d78b26aa9d53c97294b27afd5e
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-14 19:13:32 -0800

    Move Rocket

--- components/BasicIntro.tsx
+++ components/BasicIntro.tsx
@@ -7,26 +7,24 @@ gsap.registerPlugin(ScrollTrigger);
 
 export function BasicIntro() {
   useEffect(() => {
-    // Create a GSAP timeline
     const timeline = gsap.timeline();
 
-    // Define the animations
     timeline.to("#rocket", {
-      y: -400, // Move the rocketship image up by 50 pixels
-      duration: 1, // Animation duration
-      ease: "power2.inOut", // Easing function
+      y: -350,
+      duration: 1,
+      ease: "power2.inOut",
       scrollTrigger: {
         trigger: "top",
-        start: "top center", // Start the animation when the top of the rocketship is at the center of the viewport
-        end: "bottom center", // End the animation when the bottom of the rocketship is at the center of the viewport
-        scrub: true, // Smoothly transition the animation while scrolling
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
       },
     });
   }, []);
   return (
     <main>
       <div className="bg-black h-screen flex flex-col justify-center items-center">
-        <Image src="/arian_fa22.jpg" alt="Arian Izadi Portrait" width={200} height={200} className="rounded-full border-2 border-white" />
+        <Image src="/arian_fa22.jpg" alt="Arian Izadi Portrait" width={200} height={200} className="rounded-full border-2 z-10 border-white" />
         <div className="text-white text-center pt-10 px-2">
           <h1 className="text-2xl font-bold">My name is Arian!</h1>
           <p className="pt-2 max-w-xl">I am a senior studying Computer Science at the University of Nevada, Las Vegas.
