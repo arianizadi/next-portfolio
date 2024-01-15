ba7b2ab63777018963075cbf7f4de128b4fe055f
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-01-14 19:25:32 -0800

    Remove main

--- components/BasicIntro.tsx
+++ components/BasicIntro.tsx
@@ -22,15 +22,16 @@ export function BasicIntro() {
     });
   }, []);
   return (
-    <main>
-      <div className="bg-black h-screen flex flex-col justify-center items-center">
-        <Image src="/arian_fa22.jpg" alt="Arian Izadi Portrait" width={200} height={200} className="rounded-full border-2 z-10 border-white" />
-        <div className="text-white text-center pt-10 px-8">
-          <h1 className="text-2xl font-bold">My name is Arian!</h1>
-          <p className="pt-2 max-w-xl">I am a senior studying Computer Science at the University of Nevada, Las Vegas. In addition to my academic pursuits, I currently serve as the President of Layer Zero, the cybersecurity club on campus. I am always eager to explore and create innovative solutions to problems, harnessing the power of ever-changing technology to make a difference.</p>
-        </div>
-        <img className="absolute h-[200px]" id="rocket" src="/spaceship.webp" alt="spaceship" />
+    <div className="bg-black h-screen flex flex-col justify-center items-center">
+      <Image src="/arian_fa22.jpg" alt="Arian Izadi Portrait" width={200} height={200} className="rounded-full border-2 z-10 border-white" />
+      <div className="text-white text-center pt-10 px-8">
+        <h1 className="text-2xl font-bold">My name is Arian!</h1>
+        <p className="pt-2 max-w-xl">I am a senior studying Computer Science at the University of Nevada, Las Vegas. In addition to my academic pursuits, I currently serve as the President of Layer Zero, the cybersecurity club on campus. I am always eager to explore and create innovative solutions to problems, harnessing the power of ever-changing technology to make a difference.</p>
       </div>
-    </main>
+      <img className="absolute h-[200px]" id="rocket" src="/spaceship.webp" alt="spaceship" />
+      <div>
+
+      </div>
+    </div>
   )
 }
